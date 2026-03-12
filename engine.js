b93bdc777e6cb7f837eaa670b8e3c04dbbf901d0
ChronicInsanity <83089933+ChronicInsanity@users.noreply.github.com>
2026-03-12 21:31:38 +0000

    Add files via upload

--- engine.js
+++ engine.js
@@ -1,7 +1,7 @@
 
 // SETTINGS ////////////////////////////////////////////////////////
 var FILE = "game.tw";
-var VIDEO_PATH = "https://drive.google.com/file/d/";
+var VIDEO_PATH = "https://drive.google.com/uc?id=";
 
 //once you reach the end of the clip select the first choice (true)
 //if false disables the bar
